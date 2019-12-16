96095beb6d8c8b0281186804ee1ec33cb0c4de86
Diego Rodríguez Paiva <40219882+DiegoRodriguez93@users.noreply.github.com>
2019-12-16 07:11:44 -0300

    Trabajo del domingo

--- www/js/functions.js
+++ www/js/functions.js
@@ -14,3 +14,43 @@ function joinNow(){
     $('#ion-register').show();
 
 }
+function startRegister(){
+    $('#ion-register').hide();
+    $('#ion-register-1').show();
+}
+
+
+function goToStepOne(){
+    $('#ion-register-1').hide();
+    $('#ion-step-1').show();
+}
+
+function stepOne($param){
+
+    localStorage.setItem('stepone',$param);
+    $('#ion-step-1').hide();
+    $('#ion-step-2').show();
+
+}
+
+function stepTwo($param){
+
+    localStorage.setItem('steptwo',$param);
+    $('#ion-step-2').hide();
+    $('#ion-step-3').show();
+
+}
+function stepThree($param){
+
+    localStorage.setItem('stepthree',$param);
+    $('#ion-step-3').hide();
+    $('#ion-step-4').show();
+
+}
+function stepFour($param){
+
+    localStorage.setItem('stepfour',$param);
+    $('#ion-step-4').hide();
+     $('#ion-index-descubre').show(); 
+
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
 
+     $('#ion-slides').hide();
+
     $('#ion-login').hide();
+
     $('#ion-register').hide();
+
+    $('#ion-register-1').hide();
+
+    $('#ion-step-1').hide();
+    $('#ion-step-2').hide();
+    $('#ion-step-3').hide();
+    $('#ion-step-4').hide();
     
-    $('#ion-slides').show();
+    $('#ion-index-descubre').hide();
+    $('#ion-profile').show();
+    
+    
+
     
 });
